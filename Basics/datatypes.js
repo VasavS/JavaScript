473c530afb7572363a16691dccc284f06ca3da32
VasavS <140554693+VasavS@users.noreply.github.com>
2024-02-28 17:09:52 +0000

    datatypes in JS

---
+++ Basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"//treat all JS code as new version of JS.
+//The engine of JS is inside a browser.
+//alert(3+3)//we are using node.js and not a browser
+console.log(3+3)
+console.log("Vasav")
+//Datatypes in javascript.
+let name = "Vasav";//String
+let age = 23;//Integer or number
+let isLoggedIn = true;//Boolean data type
+let state = undefined;//undefined=>value is not assigned
+/*
+Primitive Data Types:
+number
+bigint->used for storing large numbers and is very rarely used
+string=>""
+boolean:true or false
+null=> standalone value and representation of any empty value and can be assigned to any variable
+undefined
+symbol->used to define unique components in React.
+
+User Defined Data Types.
+*/
+console.log(typeof 1);//number
+console.log(typeof "String");//string
+console.log(typeof true);//boolean
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
